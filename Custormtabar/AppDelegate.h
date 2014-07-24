3642b1f3062e488177943be7a0cf20cf6a7b0b4a
SD0025A <chenliang@SD0025AdeMacBook-Air.local>
2014-07-24 10:30:08 +0800

    Initial Commit

---
+++ Custormtabar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Custormtabar
+//
+//  Created by SD0025A on 14-7-24.
+//  Copyright (c) 2014年 slx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Custormtabar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Custormtabar
+//
+//  Created by SD0025A on 14-7-24.
+//  Copyright (c) 2014年 slx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
